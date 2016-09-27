1213d5c9a820d8fe2adc5013b1851c6ade341b00
Cory Robinson <crobinson42@users.noreply.github.com>
2016-09-26 20:19:19 -0700

    Js docs (#28)
    * fix(test): demo fix testing semantic-release
    
    * test(update.test.js): comment out unused code
    
    * docs(): small docs changes
    
    * test(): add custom mongodb query test

--- test/find.test.js
+++ test/find.test.js
@@ -92,4 +92,13 @@ describe('MongoDBAdapter#find', function () {
       assert.objectsEqual(userList, [{ _id: id, name: 'John' }])
     })
   })
+
+  it('should allow use of node-mongodb-native via adapter.client', function () {
+    return adapter.client.then((db) => {
+      assert.ok(db.collection('user'))
+    })
+    .catch((err) => {
+      throw new Error(err)
+    })
+  })
 })
