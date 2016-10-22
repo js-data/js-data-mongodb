03628a176ea0d6792b444a41bdda59afde3fac6c
cory robinson <coryrobinson42@gmail.com>
2016-10-22 10:43:48 -0700

    test(): add test for default option ignoreUndefined

--- test/update.test.js
+++ test/update.test.js
@@ -1,25 +1,7 @@
+import * as JSData from 'js-data'
+
 describe('MongoDBAdapter#find', function () {
   var adapter
-
-  // create a record to test update against
-  before(function () {
-    // var id
-
-    // adapter = this.$$adapter
-    // User = this.$$user
-
-    // return adapter.findAll(User, {
-    //   name: 'John'
-    // }).then(function (users) {
-    //   assert.equal(users.length, 0)
-    //   return adapter.create(User, {name: 'John'})
-    // }).then(function (user) {
-    //   id = user._id
-    //   return adapter.find(User, id.toString())
-    // }).then(function (user) {
-    //   assert.objectsEqual(user, {_id: id, name: 'John'})
-    // })
-  })
 
   beforeEach(function () {
     adapter = this.$$adapter
@@ -28,5 +10,34 @@ describe('MongoDBAdapter#find', function () {
 
   it('should not support updateMany', function () {
     return assert.throws(adapter.updateMany)
+  })
+
+  it('should ignore undefined when resource has schema', function () {
+    var id
+
+    const schema = new JSData.Schema({
+      'type': 'object',
+      'properties': {
+        'name': {
+          'type': 'string'
+        },
+        'b': {
+          'type': 'string'
+        }
+      }
+    })
+    const User = this.$$store.defineMapper('user', { schema: schema })
+
+    return adapter.findAll(User, {
+      name: 'John'
+    }).then(function (users) {
+      assert.equal(users.length, 0)
+      return adapter.create(User, {name: 'John'})
+    }).then(function (user) {
+      id = user._id
+      return adapter.find(User, id.toString())
+    }).then(function (user) {
+      assert.objectsEqual(user, {_id: id, name: 'John'})
+    })
   })
 })
